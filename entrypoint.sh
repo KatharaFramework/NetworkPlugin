0ea75b202974ecd50541e47f9be2501c6d9e9514
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-10-22 15:05:36 +0200

    Compile iptables from source to move the xtables.lock in /tmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,12 @@
 set -e
 set -x
 
+if [ ! -f "/tmp/xtables.lock" ]; then
+  if [ -f "/host_run/xtables.lock" ]; then
+    ln -s /host_run/xtables.lock /tmp/xtables.lock
+  else
+	  touch /tmp/xtables.lock && chmod 600 /tmp/xtables.lock && chown root:root /tmp/xtables.lock
+  fi
+fi
+
 ./katharanp
