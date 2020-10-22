aa776c083cb9e6047bab030f757c3744918d08f3
Tommaso Caiazzi <tommasocaiazzi@gmai.com>
2020-10-22 16:53:25 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,16 @@
 set -e
 set -x
 
-if [ ! -f "/host_etc/nftables.conf" ]; then
+/host_sbin/iptables --version | grep 'nf_tables' &> /dev/null
+if [ $? == 0 ]; then
+  update-alternatives --set iptables /usr/sbin/iptables-nft
+  update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
+else
   update-alternatives --set iptables /usr/sbin/iptables-legacy
   update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-  update-alternatives --set arptables /usr/sbin/arptables-legacy
-  update-alternatives --set ebtables /usr/sbin/ebtables-legacy
-
   if [ ! -f "/run/xtables.lock" ]; then
     ln -s /host_run/xtables.lock /run/xtables.lock
   fi
-else
-  update-alternatives --set iptables /usr/sbin/iptables-nft
-  update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
-  update-alternatives --set arptables /usr/sbin/arptables-nft
-  update-alternatives --set ebtables /usr/sbin/ebtables-nft
 fi
 
 ./katharanp
