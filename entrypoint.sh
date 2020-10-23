5537c1404c2f528443e7367275c4439f7edfacd3
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-10-23 16:52:42 +0200

    Better entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,13 @@ if [ $? == 0 ]; then
   host_iptables_path=$(readlink $container_iptables_path)
 fi
 
-# If the host iptables executable does not contain "nft", switch "legacy" into the container
-echo $host_iptables_path | grep -v 'nft' &> /dev/null
-if [ $? == 0 ]; then
+# If the host iptables executable contains "xtables" or does not contain "nft", switch "legacy" into the container
+echo $host_iptables_path | grep 'xtables' &> /dev/null
+use_xtables=$?
+echo $host_iptables_path | grep 'nft' &> /dev/null
+use_nft=$?
+
+if [ $use_xtables == 0 ] || [ $use_nft != 0 ]; then
   update-alternatives --set iptables /usr/sbin/iptables-legacy
   update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
