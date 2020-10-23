338f976ad23b75a6f7d6540022f4a3c3cd0c8049
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-10-23 12:53:09 +0200

    Choose iptables version from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 set -e
-set -x
 
-/host_sbin/iptables --version | grep 'nf_tables' &> /dev/null
+# Read where the host iptables points
+host_iptables_path=$(readlink /host_sbin/iptables)
+echo $host_iptables_path | grep 'alternatives' &> /dev/null
+# Points to /etc/alternatives
 if [ $? == 0 ]; then
-  update-alternatives --set iptables /usr/sbin/iptables-nft
-  update-alternatives --set ip6tables /usr/sbin/ip6tables-nft
-else
+  # Update the symlink to work into the container
+  container_iptables_path=$(echo $host_iptables_path | sed "s/etc/host_etc/g")
+  # Read the real iptables path
+  host_iptables_path=$(readlink $container_iptables_path)
+fi
+
+# If the host iptables executable does not contain "nft", switch "legacy" into the container
+echo $host_iptables_path | grep 'nft' &> /dev/null
+if [ $? != 0 ]; then
   update-alternatives --set iptables /usr/sbin/iptables-legacy
   update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+  # Link the host xtables.lock into the container /run
   if [ ! -f "/run/xtables.lock" ]; then
     ln -s /host_run/xtables.lock /run/xtables.lock
   fi
