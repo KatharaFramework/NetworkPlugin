b19de612556c3b8e0c729655c2c966c5a4fd90c3
Mariano Scazzariello <marianoscazzariello@gmail.com>
2020-10-27 14:57:53 +0100

    Conditionally mount xtables.lock and detect iptables from Go

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
 set -x
-
-# Read where the host iptables points
-host_iptables_path=$(readlink /host_sbin/iptables)
-echo $host_iptables_path | grep 'alternatives' &> /dev/null
-# Points to /etc/alternatives
-if [ $? == 0 ]; then
-  # Update the symlink to work into the container
-  container_iptables_path=$(echo $host_iptables_path | sed "s/etc/host_etc/g")
-  # Read the real iptables path
-  host_iptables_path=$(readlink $container_iptables_path)
-fi
-
-# If the host iptables executable contains "xtables" or does not contain "nft", switch "legacy" into the container
-echo $host_iptables_path | grep 'xtables' &> /dev/null
-use_xtables=$?
-echo $host_iptables_path | grep 'nft' &> /dev/null
-use_nft=$?
-
-if [ $use_xtables == 0 ] || [ $use_nft != 0 ]; then
-  ln -sf /sbin/iptables /usr/sbin/iptables
-
-  # Link the host xtables.lock into the container /run
-  if [ ! -f "/run/xtables.lock" ]; then
-    ln -s /host_run/xtables.lock /run/xtables.lock
-  fi
-fi
-
-if [ $use_nft == 0 ]; then
-  ln -sf /usr/sbin/iptables-nft /usr/sbin/iptables
-fi
+set -e
 
 ./katharanp
